26e9ddbe3dccaf94303f23995245be1b155762f2
Gustavo Glass <71805078+Gus-22@users.noreply.github.com>
2024-04-03 06:12:46 -0600

    Create código_aula_1.cpp

---
+++ código_aula_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int nmax = 0;
+  int soma, i;
+  
+  
+  for (i = 1; i <= nmax ; i++ )
+  {
+    soma = soma + i*i;
+  }
+  cout << "soma = " << soma << endl;
+  return 0;
+}
